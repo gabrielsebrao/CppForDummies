a867b674006d41bd131b9d8cccbb1ba3fe8d740e
gabrielsebrao <gabrielsebrao06@gmail.com>
2024-05-01 20:14:00 -0300

    07.16 - Concatenação Strings

---
+++ Parte 4/07.16 - Concatenação Strings/strings.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+int main() {
+    std::string name = "Gabriel";
+    std::string message = "Olá, ";
+    std::string carta = message + name;
+
+    std::cout << carta << '\n';
+
+    //so fiz isso pra lembrar que o c++ ajuda muito a minha vida e n preciso das tecnicas arcaicas do c
+}
